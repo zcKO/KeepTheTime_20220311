64d279b4b6378037abd06c230e086984da829546
zcKO <studyjchub@gmail.com>
2022-03-11 15:39:59 +0900

    어느 서버를 기반으로 하는 API 통신인지 명시 - BASE_URL

--- app/src/main/java/com/jc/keepthetime_20220311/api/ServerApi.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/api/ServerApi.kt
@@ -12,11 +12,15 @@ class ServerApi {
         // 서버 통신을 담당하는 클래스 ; Retrofit 클래스 객체를 담을 변수
         // 아직 안만들었다면 새로 만들고, 만들어둔게 있다면 있는 retrofit 재활용
         private var retrofit: Retrofit? = null
+        private const val BASE_URL = "https://keepthetime.xyz"
 
         fun getRetrofit(): Retrofit {
 
+            // Retrofit 라이브러리는, 클래스 차원에서 BASE_URL 을 설정할 수 있게 도와준다.
+
             if (retrofit == null) {
                 retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)      // 어느 서버를 기반으로 움직일 것인지 설정.
                     .build()
             }
 
