a47e5d48d326dfaba4352853c2eec7480a787e0e
zcKO <studyjchub@gmail.com>
2022-03-11 14:51:50 +0900

    로그인 화면 추가

---
+++ app/src/main/java/com/jc/keepthetime_20220311/SignInActivity.kt
@@ -0,0 +1,18 @@
+package com.jc.keepthetime_20220311
+
+import android.os.Bundle
+
+class SignInActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
+    }
+}
