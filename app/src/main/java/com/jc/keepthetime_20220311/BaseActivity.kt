2aa7a7a104f68527a97b88d375ae3855dc5b11b9
zcKO <studyjchub@gmail.com>
2022-03-11 15:58:29 +0900

    Retrofit 클래스 객체 / APIList 인터페이스 결합 => apiList 인터페이스 변수 상속 처리.  - 모든 화면에서 apiList."서버기능()" 형태로 서버 통신

--- app/src/main/java/com/jc/keepthetime_20220311/BaseActivity.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/BaseActivity.kt
@@ -3,14 +3,24 @@ package com.jc.keepthetime_20220311
 import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.jc.keepthetime_20220311.api.APIList
+import com.jc.keepthetime_20220311.api.ServerApi
+import retrofit2.create
 
 abstract class BaseActivity : AppCompatActivity() {
 
     lateinit var mContext: Context
 
+    // 모든 화면에서, apiList 변수가 있다면 => apiList."서버기능()" 형태로 간단히 코딩이 가능하다.
+    lateinit var apiList: APIList
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mContext = this
+
+        val retrofit = ServerApi.getRetrofit()
+        apiList = retrofit.create(APIList::class.java)
+
     }
 
     abstract fun setupEvents()
