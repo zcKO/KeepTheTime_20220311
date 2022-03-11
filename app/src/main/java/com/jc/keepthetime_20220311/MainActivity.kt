345b0fa410e88a29f50402a4a209586e0e8bd480
zcKO <studyjchub@gmail.com>
2022-03-11 14:48:38 +0900

    메인화면 바인딩 처리

--- app/src/main/java/com/jc/keepthetime_20220311/MainActivity.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/MainActivity.kt
@@ -2,10 +2,15 @@ package com.jc.keepthetime_20220311
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.jc.keepthetime_20220311.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
     }
 }
