0cda3d6d5d237f6829a19984344422e02c0f6c6e
zcKO <studyjchub@gmail.com>
2022-03-11 17:04:50 +0900

    응답이 성공일 경우만, 토스트 띄우기

--- app/src/main/java/com/jc/keepthetime_20220311/SignUpActivity.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/SignUpActivity.kt
@@ -1,6 +1,7 @@
 package com.jc.keepthetime_20220311
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.jc.keepthetime_20220311.databinding.ActivitySignUpBinding
 import com.jc.keepthetime_20220311.datas.BasicResponse
@@ -35,7 +36,12 @@ class SignUpActivity : BaseActivity() {
                         call: Call<BasicResponse>,
                         response: Response<BasicResponse>
                     ) {
-                        
+
+                        if (response.isSuccessful) {
+                            Toast.makeText(mContext, "회원가입에 성공했습니다!", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+
                     }
 
                     override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
