eab0609c13f9207f6728d538db50a332a869ced5
zcKO <studyjchub@gmail.com>
2022-03-11 16:16:02 +0900

    응답에 "성공 / 실패" 분기 나누기

--- app/src/main/java/com/jc/keepthetime_20220311/SignInActivity.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/SignInActivity.kt
@@ -31,6 +31,19 @@ class SignInActivity : BaseActivity() {
             apiList.postRequestLogin(inputEmail, inputPassword).enqueue(object: Callback<JSONObject> {
                 override fun onResponse(call: Call<JSONObject>, response: Response<JSONObject>) {
                     Log.d("서버응답", response.toString())
+
+                    // retrofit 라이브러리의 response 는, "성공 / 실패" 여부에 따라 다른 본문을 봐야한다.
+                    // 성공인지
+                    if (response.isSuccessful) {
+                        // 모든 결과가 최종 성공인 경우. (code = 200 으로 내려옴)
+                        // response.body() 이용
+                    } else {
+                        // 실패인지
+                        // 서버 연결은 되어 있지만, 로직만 실패. (로그인 비번 틀림 등)
+                        // response.errorBody() 활용.
+                    }
+
+
                 }
 
                 override fun onFailure(call: Call<JSONObject>, t: Throwable) {
