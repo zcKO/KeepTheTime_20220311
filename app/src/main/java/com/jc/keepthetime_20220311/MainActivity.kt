c76c73ba9faf53a40f8c42755427c1e721c2757b
zcKO <studyjchub@gmail.com>
2022-03-11 14:50:16 +0900

    메인 화면 > BaseActivity 상속

--- app/src/main/java/com/jc/keepthetime_20220311/MainActivity.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/MainActivity.kt
@@ -1,16 +1,23 @@
 package com.jc.keepthetime_20220311
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
 import com.jc.keepthetime_20220311.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setValues() {
+
     }
 }
