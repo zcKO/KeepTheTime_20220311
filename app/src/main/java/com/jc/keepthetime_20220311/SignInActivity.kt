4587f3db71cac04f7e2aaae479a15c07bf1aa245
zcKO <studyjchub@gmail.com>
2022-03-11 16:38:17 +0900

    주석 추가 ( runOnUiThread )

--- app/src/main/java/com/jc/keepthetime_20220311/SignInActivity.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/SignInActivity.kt
@@ -2,6 +2,7 @@ package com.jc.keepthetime_20220311
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.jc.keepthetime_20220311.databinding.ActivitySignInBinding
 import com.jc.keepthetime_20220311.datas.BasicResponse
@@ -38,6 +39,13 @@ class SignInActivity : BaseActivity() {
                     if (response.isSuccessful) {
                         // 모든 결과가 최종 성공인 경우. (code = 200 으로 내려옴)
                         // response.body() 이용
+
+                        val br = response.body()!!       // 성공시 무조건 본문이 있다. => BasicResponse 형태의 변수로 파싱되어 나온다.
+
+                        // Retrofit 의 Callback 은 UIThread 안으로 다시 돌아오도록 처리되어 있다.
+                        // UI 조작을 위해 runOnUiThread { } 작성이 필요 없다.
+                        Toast.makeText(mContext, br.message, Toast.LENGTH_SHORT).show()
+
                     } else {
                         // 실패인지
                         // 서버 연결은 되어 있지만, 로직만 실패. (로그인 비번 틀림 등)
