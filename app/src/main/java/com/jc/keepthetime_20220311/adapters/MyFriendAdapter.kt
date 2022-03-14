585c4fe66645e3565a2b2277df535e1d96836040
zcKO <studyjchub@gmail.com>
2022-03-14 14:44:34 +0900

    리스트뷰 어댑터 뼈대 작업 마무리

--- app/src/main/java/com/jc/keepthetime_20220311/adapters/MyFriendAdapter.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/adapters/MyFriendAdapter.kt
@@ -2,7 +2,11 @@ package com.jc.keepthetime_20220311.adapters
 
 import android.content.Context
 import android.service.autofill.UserData
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.jc.keepthetime_20220311.R
 
 class MyFriendAdapter(
     val mContext: Context,
@@ -10,6 +14,18 @@ class MyFriendAdapter(
     val mList: List<UserData>
 ): ArrayAdapter<UserData>(mContext, resId, mList) {
 
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
 
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.my_friend_list_item, null)
+        }
+
+        val row = tempRow!!
+
+
+        return row
+    }
 
 }
