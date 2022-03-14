90fb41127b5eadddd7a5ee072cfaa122e2cb8d6c
zcKO <studyjchub@gmail.com>
2022-03-14 10:08:31 +0900

    응답 내용 파싱 > UI  반영  - 로그인에서 파싱 했던 구조와 동일하게 파싱됨.  - 미리 파싱해둔 구조를 재활용

--- app/src/main/java/com/jc/keepthetime_20220311/MainActivity.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/MainActivity.kt
@@ -32,6 +32,15 @@ class MainActivity : BaseActivity() {
         apiList.getRequestMyInfo(ContextUtil.getLoginUserToken(mContext)).enqueue( object: Callback<BasicResponse> {
             override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
 
+                if(response.isSuccessful) {
+
+                    // JSON 응답을 서버에서 보고, 파싱부터 진행 => 파싱된 변수들을 활용.
+                    // 이미 파싱 해둔 구조를 재활용.
+                    val br = response.body()!! // 타이핑을 덜 하기 위해 옮겨 담는 변수
+                    binding.txtUserNickname.text = br.data.user.nick_name
+
+                }
+
             }
 
             override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
--- app/src/main/java/com/jc/keepthetime_20220311/datas/DataResponse.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/datas/DataResponse.kt
@@ -1,6 +1,10 @@
 package com.jc.keepthetime_20220311.datas
 
 // 서버 응답중, data: {} 를 파싱하기 위한 클래스
+
+// 여러 API 에서 data: {} 를 내려줄 예정 => 내부 구성 모양이 각각 다를 수 있다.
+// ex. user: {} 만 있다던지
+// 모든 경우에 대해 전부 변수 생성 => 활용시에만 주의하면 된다.
 class DataResponse(
     val user: UserData,
     val token: String
