fd087f07349df994c96ea8dbe6977005d3002e97
zcKO <studyjchub@gmail.com>
2022-03-14 16:08:37 +0900

    리사이클러뷰의 어댑터 제작중  - 필수 오버라이딩 함수 3개 생성  - 몇줄 짜리 리사이클러뷰인지 : getItemCount()  - 어댑터의 생성자로 Context, List 를 받아서 활용

--- app/src/main/java/com/jc/keepthetime_20220311/adapters/SearchedUserRecyclerAdapter.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/adapters/SearchedUserRecyclerAdapter.kt
@@ -1,12 +1,29 @@
 package com.jc.keepthetime_20220311.adapters
 
+import android.content.Context
 import android.view.View
+import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.jc.keepthetime_20220311.datas.UserData
 
-class SearchedUserRecyclerAdapter : RecyclerView.Adapter<SearchedUserRecyclerAdapter.MyViewHolder>() {
+class SearchedUserRecyclerAdapter(
+    val mContext: Context,
+    val mList: List<UserData>
+) : RecyclerView.Adapter<SearchedUserRecyclerAdapter.MyViewHolder>() {
 
     inner class MyViewHolder(view: View): RecyclerView.ViewHolder(view) {
 
     }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+    }
+
+    // 몇 개의 아이템을 보여줄 것인지 => 데이터 목록의 갯 수 만큼 보여준다.
+    override fun getItemCount(): Int = mList.size
 
 }
