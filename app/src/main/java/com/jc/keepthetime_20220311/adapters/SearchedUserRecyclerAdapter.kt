dc528f9b712a633c07c79a5434e47d3f7172d669
zcKO <studyjchub@gmail.com>
2022-03-14 16:04:46 +0900

    리사이클러뷰의 어댑터 제작중  - RecyclerView.Adapter 상속  - inner class 로 MyViewHolder 생성  - 어댑터가 다룰 항목으로 MyViewHolder 선정

---
+++ app/src/main/java/com/jc/keepthetime_20220311/adapters/SearchedUserRecyclerAdapter.kt
@@ -0,0 +1,12 @@
+package com.jc.keepthetime_20220311.adapters
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class SearchedUserRecyclerAdapter : RecyclerView.Adapter<SearchedUserRecyclerAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(view: View): RecyclerView.ViewHolder(view) {
+
+    }
+
+}
