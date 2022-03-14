9d8515d1f554a3761ae89df2514bbadb6098dc66
zcKO <studyjchub@gmail.com>
2022-03-14 11:53:21 +0900

    자동 로그인 기능 구현 완료  - 임시 : 내 정보 조회 성공 여부만 체크.

--- app/src/main/java/com/jc/keepthetime_20220311/SplashActivity.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/SplashActivity.kt
@@ -4,6 +4,11 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import android.os.Looper
+import com.jc.keepthetime_20220311.datas.BasicResponse
+import com.jc.keepthetime_20220311.utils.ContextUtil
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SplashActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,13 +26,40 @@ class SplashActivity : BaseActivity() {
 
     override fun setValues() {
 
-        // 임시 - 자동 로그인 구현 전까지는, 2.5 초 후에 무조건 로그인화면으로.
+        // 실제 - 저장된 토큰으로, 내 정보 조회 시도 먼저 진행
+        // 2.5 초후에, 내 정보가 불러와졌는지 확인 그 결과에 따라 다른 화면으로 이동.
+
+        var isMyInfoLoaded = false
+
+        apiList.getRequestMyInfo(ContextUtil.getLoginUserToken(mContext)).enqueue(object : Callback<BasicResponse> {
+            override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
+
+                // CODE : 200 => 성공 응답이 왔다면 내 정보가 일단 잘 불려졌다.
+                if (response.isSuccessful) {
+                    isMyInfoLoaded = true
+                }
+            }
+
+            override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+                call.cancel()
+            }
+
+        })
+
         val myHandler = Handler(Looper.getMainLooper())
 
         myHandler.postDelayed({
-            val myIntent = Intent(mContext, SignInActivity::class.java)
+
+            val myIntent: Intent
+
+            if (isMyInfoLoaded) {
+                myIntent = Intent(mContext, MainActivity::class.java)
+            } else {
+                myIntent = Intent(mContext, SignInActivity::class.java)
+            }
             startActivity(myIntent)
             finish()
+
         }, 2500)
 
     }
