9685f8252162a3369fbcc58f85acfd94630569e3
zcKO <studyjchub@gmail.com>
2022-03-15 10:35:52 +0900

    프래그먼트 / 뷰페이저 / 탭 레아이웃 연동 완료

--- app/src/main/java/com/jc/keepthetime_20220311/ManageMyFriendsActivity.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/ManageMyFriendsActivity.kt
@@ -3,11 +3,14 @@ package com.jc.keepthetime_20220311
 import android.content.Intent
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
+import com.jc.keepthetime_20220311.adapters.FriendViewPagerAdapter
 import com.jc.keepthetime_20220311.databinding.ActivityManageMyFriendsBinding
 
 class ManageMyFriendsActivity : BaseActivity() {
 
     lateinit var binding: ActivityManageMyFriendsBinding
+
+    lateinit var mAdapter: FriendViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,6 +34,11 @@ class ManageMyFriendsActivity : BaseActivity() {
     }
 
     override fun setValues() {
+
+        mAdapter = FriendViewPagerAdapter(supportFragmentManager)
+        binding.friendViewPager.adapter = mAdapter
+
+        binding.friendsTabLayout.setupWithViewPager(binding.friendViewPager)
 
     }
 
---
+++ app/src/main/java/com/jc/keepthetime_20220311/adapters/FriendViewPagerAdapter.kt
@@ -0,0 +1,37 @@
+package com.jc.keepthetime_20220311.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import com.jc.keepthetime_20220311.fragments.AppointmentListFragment
+import com.jc.keepthetime_20220311.fragments.MyFriendsFragment
+import com.jc.keepthetime_20220311.fragments.MyProfileFragment
+import com.jc.keepthetime_20220311.fragments.RequestedUsersFragment
+
+class FriendViewPagerAdapter(
+    fm: FragmentManager
+): FragmentPagerAdapter(fm) {
+
+    override fun getCount(): Int = 2
+
+    override fun getItem(position: Int): Fragment {
+
+        return when(position) {
+            0 -> {
+                MyFriendsFragment()
+            }
+            else -> {
+                RequestedUsersFragment()
+            }
+        }
+
+    }
+
+    override fun getPageTitle(position: Int): CharSequence? {
+        return when(position) {
+            0 -> {"내 친구 목록"}
+            else -> {"친구 요청 확인"}
+        }
+    }
+
+}
