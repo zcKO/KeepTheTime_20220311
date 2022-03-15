232630f584ce268628949d673100f8fef229fe2a
zcKO <studyjchub@gmail.com>
2022-03-15 12:01:56 +0900

    메인화면 리셋 - ViewPager2 / BottomNavigation 으로 구조 변경 예정

--- app/src/main/java/com/jc/keepthetime_20220311/MainActivity.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/MainActivity.kt
@@ -2,19 +2,11 @@ package com.jc.keepthetime_20220311
 
 import android.os.Bundle
 import androidx.databinding.DataBindingUtil
-import com.jc.keepthetime_20220311.adapters.MainViewPagerAdapter
 import com.jc.keepthetime_20220311.databinding.ActivityMainBinding
-import com.jc.keepthetime_20220311.datas.BasicResponse
-import com.jc.keepthetime_20220311.utils.ContextUtil
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : BaseActivity() {
 
     lateinit var binding: ActivityMainBinding
-
-    lateinit var mAdapter: MainViewPagerAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,10 +21,6 @@ class MainActivity : BaseActivity() {
     }
 
     override fun setValues() {
-        mAdapter = MainViewPagerAdapter(supportFragmentManager)
-        binding.mainViewPager.adapter = mAdapter
-
-        binding.mainTabLayout.setupWithViewPager(binding.mainViewPager)
 
     }
 }
--- app/src/main/java/com/jc/keepthetime_20220311/adapters/RequestUserRecyclerAdapter.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/adapters/RequestUserRecyclerAdapter.kt
@@ -8,6 +8,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.jc.keepthetime_20220311.R
