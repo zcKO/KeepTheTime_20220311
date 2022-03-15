e9f6b398807dc7b7063ce6b84fa9b56ce73ff504
zcKO <studyjchub@gmail.com>
2022-03-15 11:44:47 +0900

    두개의 버튼이 눌리면 할 일을, 하나의 코드로 표현  - tag 속성에 각 버튼의 메모를 남겨서 활용.

--- app/src/main/java/com/jc/keepthetime_20220311/adapters/RequestUserRecyclerAdapter.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/adapters/RequestUserRecyclerAdapter.kt
@@ -1,9 +1,11 @@
 package com.jc.keepthetime_20220311.adapters
 
 import android.content.Context
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
@@ -22,6 +24,8 @@ class RequestUserRecyclerAdapter(
         val txtNickname = view.findViewById<TextView>(R.id.txtNickname)
         val imgSocialLoginLogo = view.findViewById<ImageView>(R.id.imgSocialLoginLogo)
         val txtEmail = view.findViewById<TextView>(R.id.txtEmail)
+        val btnAccept = view.findViewById<Button>(R.id.btnAccept)
+        val btnDeny = view.findViewById<Button>(R.id.btnDeny)
 
         fun bind(data: UserData) {
 
@@ -57,8 +61,27 @@ class RequestUserRecyclerAdapter(
                 }
             }
 
-        }
+            // btnAccept, btnDeny 모두 같은 API 를 호출. (하는 행동이 같다.)
+            //  => type 파라미터에 첨부하는 값만 다르다. ("수락"/ "거절")
 
+            // 두 개의 버튼이 눌리면, 할 일을 하나의 변수에 담아두자 (같은 일을 하게 만든다.)
+            // 할 일 : Interface => 정석 - object : 인터페이스 종류 { }
+            // 축약 문법 (lambda) => 인터페이스 종류 { }
+            val ocl = View.OnClickListener {
+                // 서버에 수락 / 거절 의사 전달.
+                // 수락 버튼 : 수락, 거절 버튼 : 거절 => 어느 버튼이 눌렸는지 파악 가능해야, 파라미터도 다르게 전달 할 수 있다.
+                // it 변수 : 클릭된 버튼을 담고 있는 역할.
+                // tag 속성 : 아무 말이나 적어도 되는 일종의 메모. 수락 / 거절 등 보내야할 값을 메모해두자
+
+                val tagStr = it.tag.toString()
+                Log.d("보낼파라미터값", tagStr)
+
+            }
+
+            btnAccept.setOnClickListener(ocl)
+            btnDeny.setOnClickListener(ocl)
+
+        }
 
     }
 
