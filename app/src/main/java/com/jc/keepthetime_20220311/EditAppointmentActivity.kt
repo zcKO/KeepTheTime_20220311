c138aaacd327d2224d5decb917cc5a5770a0dffa
zcKO <studyjchub@gmail.com>
2022-03-15 15:54:21 +0900

    시간 선택 팝업창 띄워보기  - DatePickerDialog 와 거의 유사하다.

--- app/src/main/java/com/jc/keepthetime_20220311/EditAppointmentActivity.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/EditAppointmentActivity.kt
@@ -1,8 +1,10 @@
 package com.jc.keepthetime_20220311
 
 import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.os.Bundle
 import android.widget.DatePicker
+import android.widget.TimePicker
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.jc.keepthetime_20220311.databinding.ActivityEditAppointmentBinding
@@ -27,6 +29,7 @@ class EditAppointmentActivity : BaseActivity() {
 
     override fun setupEvents() {
 
+        // 날짜 선택 텍스트뷰 클릭 이벤트 - DatePickerDialog
         binding.txtDate.setOnClickListener {
             val dsl = object : DatePickerDialog.OnDateSetListener {
 
@@ -51,6 +54,25 @@ class EditAppointmentActivity : BaseActivity() {
                 mSelectedAppointmentDateTime.get(Calendar.MONTH),
                 mSelectedAppointmentDateTime.get(Calendar.DAY_OF_MONTH)
             ).show()
+        }
+
+        // 시간 선택 텍스트뷰 클릭 이벤트 - TimePickerDialog
+        binding.txtTime.setOnClickListener {
+            val tsl = object : TimePickerDialog.OnTimeSetListener {
+                override fun onTimeSet(p0: TimePicker?, hourOfDay: Int, minute: Int) {
+                    Toast.makeText(mContext, "${hourOfDay}시 ${minute}분 선택", Toast.LENGTH_SHORT).show()
+                }
+
+            }
+
+            val tpd = TimePickerDialog(
+                mContext,
+                tsl,
+                18,
+                0,
+                false
+            ).show()
+
         }
 
     }
