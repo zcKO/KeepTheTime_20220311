f19e6d422f53b36285a08836a219962b39f96763
zcKO <studyjchub@gmail.com>
2022-03-15 15:39:10 +0900

    날짜 선택시 - Calendar / SimpeDateFormat 으로 월 값 보정.

--- app/src/main/java/com/jc/keepthetime_20220311/EditAppointmentActivity.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/EditAppointmentActivity.kt
@@ -6,10 +6,15 @@ import android.widget.DatePicker
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.jc.keepthetime_20220311.databinding.ActivityEditAppointmentBinding
+import java.text.SimpleDateFormat
+import java.util.*
 
 class EditAppointmentActivity : BaseActivity() {
 
     lateinit var binding: ActivityEditAppointmentBinding
+
+    // 약속 시간 일/시 를 저장해줄 Calendar (월 값이 0 ~ 11 로 움직이게 맞춰져있다.)
+    val mSelectedAppointmentDateTime = Calendar.getInstance() // 기본 값: 현재 일시
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -26,8 +31,15 @@ class EditAppointmentActivity : BaseActivity() {
             val dsl = object : DatePickerDialog.OnDateSetListener {
 
                 override fun onDateSet(p0: DatePicker?, year: Int, month: Int, dayOfMonth: Int) {
+                    // 연/월/일은, JAVA / Kotlin 언어의 기준 (0 ~ 11월)으로 월 값을 준다. (사람은 1 ~ 12월)
+                    // 주는 그대로 Calendar 에 set 하게 되면, 올바른 월로 세팅된다.
+                    mSelectedAppointmentDateTime.set(year, month, dayOfMonth)   // 연월일 한번에 세팅하는 함수
 
-                    Toast.makeText(mContext, "${year}년 ${month}월 ${dayOfMonth}일 선택함", Toast.LENGTH_SHORT).show()
+                    // 약속 일자의 문구를 22/03/08 등의 형식으로 바꿔서 보여주자.
+                    // SimpleDateFormat 을 활용 => 월 값도 알아서 보정
+                    val sdf = SimpleDateFormat("yy/MM/dd")
+
+                    binding.txtDate.text = sdf.format(mSelectedAppointmentDateTime.time)
 
                 }
             }
