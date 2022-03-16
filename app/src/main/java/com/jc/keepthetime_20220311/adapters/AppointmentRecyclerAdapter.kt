b2dd189fddddbe6b07f674f38e85dcfe86f42f5e
zcKO <studyjchub@gmail.com>
2022-03-16 10:40:57 +0900

    약속 목록 어댑터 수정

--- app/src/main/java/com/jc/keepthetime_20220311/adapters/AppointmentRecyclerAdapter.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/adapters/AppointmentRecyclerAdapter.kt
@@ -11,23 +11,24 @@ import com.jc.keepthetime_20220311.datas.AppointmentData
 class AppointmentRecyclerAdapter(
     val mContext: Context,
     val mList: List<AppointmentData>
-): RecyclerView.Adapter<AppointmentRecyclerAdapter.MyViewHolder>() {
+) : RecyclerView.Adapter<AppointmentRecyclerAdapter.MyViewHolder>() {
 
-    inner class MyViewHolder(view: View): RecyclerView.ViewHolder(view) {
+    inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
 
         fun bind(data: AppointmentData) {
-
 
 
         }
 
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder =
+        MyViewHolder(
+            LayoutInflater
+                .from(mContext)
+                .inflate(R.layout.appointment_list_item, parent, false)
+        )
 
-        val view = LayoutInflater.from(mContext).inflate(R.layout.appointment_list_item, null)
-        return MyViewHolder(view)
-    }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
 
