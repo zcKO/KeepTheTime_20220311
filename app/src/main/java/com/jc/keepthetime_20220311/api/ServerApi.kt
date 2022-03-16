f4ccc9427376d31456d8bb4a395fe5b17bcdb3eb
zcKO <studyjchub@gmail.com>
2022-03-16 11:13:09 +0900

    파싱하려는 변수 자료형을 Date 로 넣으면 어떻게 파싱할 것인지 그 방법 명시  - retrofit 의 세팅 항목 추가

--- app/src/main/java/com/jc/keepthetime_20220311/api/ServerApi.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/api/ServerApi.kt
@@ -1,11 +1,13 @@
 package com.jc.keepthetime_20220311.api
 
 import android.content.Context
+import com.google.gson.GsonBuilder
 import com.jc.keepthetime_20220311.utils.ContextUtil
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.*
 
 class ServerApi {
 
@@ -49,9 +51,18 @@ class ServerApi {
                     .addInterceptor(interceptor)
                     .build()
 
+                // Date 자료형으로 파싱 => String 을 yyyy-MM-dd HH:mm:ss 으로 파싱해서 저장해야한다. (고정된 양식으로 내려줌)
+                val gson = GsonBuilder()
+                    .setDateFormat("yyy-MM-dd HH:mm:ss")    // 서버가 이런 양식으로 보내주는 String 을
+                    .registerTypeAdapter(
+                        Date::class.java,
+                        DateDeserializer()
+                    )     // 어떤 형태의 자료형에 적용시킬지, Date 클래스로 파싱
+                    .create()
+
                 retrofit = Retrofit.Builder()
                     .baseUrl(BASE_URL)      // 어느 서버를 기반으로 움직일 것인지 설정.
-                    .addConverterFactory(GsonConverterFactory.create())     // gson 라이브러리와 결합
+                    .addConverterFactory(GsonConverterFactory.create(gson))     // gson 라이브러리와 결합 + Date 파싱 요령 첨부
                     .client(myClient)       // 인터셉터를 부착해둔 클라이언트로 통신하도록 설정.
                     .build()
             }
--- app/src/main/java/com/jc/keepthetime_20220311/datas/AppointmentData.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/datas/AppointmentData.kt
@@ -1,10 +1,12 @@
 package com.jc.keepthetime_20220311.datas
 
+import java.util.*
+
 class AppointmentData(
     val id: Int,
     val user_id: Int,
     val title: String,
-    val datetime: String,           // 서버는 String 으로 내려주지만, 파싱은 Date 로 바꿔주고 싶다.
+    val datetime: Date,           // 서버는 String 으로 내려주지만, 파싱은 Date 로 바꿔주고 싶다.
     val start_place: String,
     val start_latitude: Double,
     val start_longitude: Double,
