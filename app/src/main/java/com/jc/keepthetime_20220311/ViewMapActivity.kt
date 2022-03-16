cfc69edf786c996af272316329dba1601e17d47e
zcKO <studyjchub@gmail.com>
2022-03-16 16:52:54 +0900

    모든 정거장 정보를 모아서 > PathOverlay 가 지나가야할 좌표로 사용.

--- app/src/main/java/com/jc/keepthetime_20220311/ViewMapActivity.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/ViewMapActivity.kt
@@ -13,6 +13,7 @@ import com.odsay.odsayandroidsdk.API
 import com.odsay.odsayandroidsdk.ODsayData
 import com.odsay.odsayandroidsdk.ODsayService
 import com.odsay.odsayandroidsdk.OnResultCallbackListener
+import kotlin.io.path.Path
 
 class ViewMapActivity : BaseActivity() {
 
@@ -38,30 +39,29 @@ class ViewMapActivity : BaseActivity() {
 
         binding.naverMapView.getMapAsync { naverMap ->
 
-            // 도착지 변수
-            val destLatLng = LatLng(mAppointment.latitude, mAppointment.longitude)
+//            도착지 자체를 변수화.
+            val destLatLng = LatLng( mAppointment.latitude,  mAppointment.longitude )
 
-            val startLatLng = LatLng(mAppointment.start_latitude, mAppointment.start_longitude)
+//            도착지로 카메라 이동
 
-            // 마커
-            val startMarker = Marker()
-            val endMarker = Marker()
-            val cameraUpdate = CameraUpdate.scrollTo(destLatLng)
-            naverMap.moveCamera(cameraUpdate)
+            val cameraUpdate = CameraUpdate.scrollTo( destLatLng )
+            naverMap.moveCamera( cameraUpdate )
 
-            startMarker.position = startLatLng
-            endMarker.position = destLatLng
+//            마커도 찍어주자.
 
-            startMarker.map = naverMap
-            endMarker.map = naverMap
-
-
-            // Path 객체의 좌표를 설정 => naverMap 에 추가
-            val path = PathOverlay()
-
-            // path.coords => 출발지 / 도착지만 넣어서 대입하면? 일직선 연결
+            val marker = Marker()
+            marker.position = destLatLng
+            marker.map = naverMap
 
             // 출발지 ~ 도착지 사이의 정거장이 있다면 정거장들을 좌표로 추가
+
+            val stationList = ArrayList<LatLng>()
+
+            // 첫 좌표는 출발 장소
+            stationList.add(LatLng(mAppointment.start_latitude, mAppointment.start_longitude))
+
+            // 거치게 되는 정거장들 목록을 > ODsayService 로 받아서 추가
+
             val myODsayService = ODsayService.init(mContext, "+49PY7ooyTk1KYzli+tMi2j8iWiI6WcC4EdkansUJz8")
 
             myODsayService.requestSearchPubTransPath(
@@ -79,7 +79,50 @@ class ViewMapActivity : BaseActivity() {
 
                         Log.d("대중교통길찾기", jsonObj.toString())
                         val resultObj = jsonObj.getJSONObject("result")
-                        Log.d("resultObj", resultObj.toString())
+                        Log.d("resultObj", jsonObj.toString())
+
+                        val pathArr = resultObj.getJSONArray("path")    // 여러 개의 추천 경로 => 0번째 경로로 사용.
+
+                        val firstPathObj = pathArr.getJSONObject(0)
+
+                        // 첫 경로의 subPath 목록 파싱 (도보 - 버스 - 지하철 - 도보 ...)
+                        val subPathArr = firstPathObj.getJSONArray("subPath")
+
+                        for (i in 0 until subPathArr.length()) {
+
+                            val subPathObj = subPathArr.getJSONObject(i)
+
+                            if (!subPathObj.isNull("passStopList")) {
+
+                                // 도보가 아니어서, 정거장 목록을 주는 경우.
+                                val passStopListObj = subPathObj.getJSONObject("passStopList")
+                                val stationsArr = passStopListObj.getJSONArray("stations")
+
+                                for (j in 0 until stationsArr.length()) {
+
+                                    val stationObj = stationsArr.getJSONObject(j)
+
+                                    val stationLat = stationObj.getString("y").toDouble()
+                                    val stationLng = stationObj.getString("x").toDouble()
+
+                                    // 네이버 지도의 경로선에 그려줄 좌표 목록에 추가.
+                                    stationList.add(LatLng(stationLat, stationLng))
+
+                                }
+
+                            }
+
+                        }
+
+                        // 모든 정거장 목록이 추가되어있다.
+                        // 마지막 경로선으로, 도착지를 추가.
+
+                        stationList.add(destLatLng)
+
+                        // 경로선을 지도에 그려주자
+                        val path = PathOverlay()
+                        path.coords = stationList
+                        path.map = naverMap
 
                     }
 
