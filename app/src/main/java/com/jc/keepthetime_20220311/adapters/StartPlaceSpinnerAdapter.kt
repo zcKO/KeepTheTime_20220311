2a8dc096d3250259093511dfde3770d9695909da
zcKO <studyjchub@gmail.com>
2022-03-17 11:43:56 +0900

    ListView처럼, ArrayAdpater를 상속받는 어댑터 클래스 추가  - 뼈대작업만 진행

---
+++ app/src/main/java/com/jc/keepthetime_20220311/adapters/StartPlaceSpinnerAdapter.kt
@@ -0,0 +1,43 @@
+package com.jc.keepthetime_20220311.adapters
+
+import android.content.Context
+import android.util.Log
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.jc.keepthetime_20220311.R
+import com.jc.keepthetime_20220311.datas.PlaceData
+import com.jc.keepthetime_20220311.datas.UserData
+
+
+class StartPlaceSpinnerAdapter(
+    val mContext: Context,
+    resId: Int,
+    val mList: List<PlaceData>
+) : ArrayAdapter<PlaceData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow =
+                LayoutInflater.from(mContext).inflate(R.layout.start_place_spinner_list_item, null)
+
+        }
+
+        val row = tempRow!!
+
+        val data = mList[position]
+
+        return row
+
+    }
+
+}
