79cf8f412912275f7f015754cc4d856d37f83f64
zcKO <studyjchub@gmail.com>
2022-03-23 10:18:37 +0900

    응답 성공시, 선택한 사진으로 UI 변경

--- app/src/main/java/com/jc/keepthetime_20220311/fragments/MyProfileFragment.kt
+++ app/src/main/java/com/jc/keepthetime_20220311/fragments/MyProfileFragment.kt
@@ -7,6 +7,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AlertDialog
 import androidx.databinding.DataBindingUtil
 import com.bumptech.glide.Glide
@@ -157,6 +158,14 @@ class MyProfileFragment : BaseFragment() {
                         call: Call<BasicResponse>,
                         response: Response<BasicResponse>
                     ) {
+
+                        if (response.isSuccessful) {
+                            Toast.makeText(mContext, "프로필사진이 변경되었습니다.", Toast.LENGTH_SHORT).show()
+                            // 선택해둔 이미지로 UI 반영
+                            Glide.with(mContext)
+                                .load(selectedImageUri)
+                                .into(binding.imgProfile)
+                        }
 
                     }
 
